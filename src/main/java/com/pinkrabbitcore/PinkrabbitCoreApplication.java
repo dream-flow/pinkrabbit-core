a8a3dd76411cdf01f2a9ac649804016574efe49b
jm <cjmfortune@gmail.com>
2024-05-20 14:42:55 +0900

    init source

---
+++ src/main/java/com/pinkrabbitcore/PinkrabbitCoreApplication.java
@@ -0,0 +1,13 @@
+package com.pinkrabbitcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkrabbitCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinkrabbitCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pinkrabbitcore/PinkrabbitCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinkrabbitcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinkrabbitCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
